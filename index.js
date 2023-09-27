ed950d2bf174f61bfda496bc8e145fc378b9b0d6
Alba Giraldo <agiraldomolina@gmail.com>
2023-09-27 12:42:24 -0400

    API limiting implemented OK

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*eslint-disable */
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const appError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -12,12 +13,22 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//MIDDLEWARES:
-/* 'Dev' is one of the predefined logging formats provided by Morgan. This logging format displays basic information about each incoming request, such as the HTTP method, the route, the status code, and the response time.*/
 console.log(process.env.NODE_ENV);
+
+// Global MIDDLEWARES
+/* 'Dev' is one of the predefined logging formats provided by Morgan. This logging format displays basic information about each incoming request, such as the HTTP method, the route, the status code, and the response time.*/
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-}
+};
+
+// Rate limiting middleware for allowing max 100 requests from the client to the server durin 1  hour
+const limiter = rateLimit({
+  max : 100,
+  windowMs : 60 * 60 * 1000,
+  message: 'To many requests from this IP, please try again in an hour.'
+});
+
+app.use('/', limiter);
 
 /* Next middleware is used to ensure that your Express application can understand and work with JSON data sent in incoming requests. This is crucial for many modern web applications, as JSON is a common format for exchanging data between the client and server in API (Application Programming Interface)-based web applications*/
 app.use(express.json());
